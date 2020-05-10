3f571a029e645a59e30446013258114c16203c39
Yusuf Caner Güreş <gurescaner@gmail.com>
2020-05-10 16:27:54 +0300

    Fragments corrected and profile,username added.

--- app/src/main/java/com/example/huaweichallange/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/huaweichallange/fragment/ProfileFragment.kt
@@ -6,22 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.huaweichallange.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ProfileFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-        //val photo = arguments?.getString("photo")
-        //val name = arguments?.getString("name")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_profile, container, false)
+        val photo = arguments?.getString("photo")
+        val name = arguments?.getString("name")
 
+        Picasso.get().load(photo).into(rootView.personPhoto)
+        rootView.personname.text = name
 
+        return rootView
     }
 
 }
--- app/src/main/java/com/example/huaweichallange/ui/TabbedActivity.kt
+++ app/src/main/java/com/example/huaweichallange/ui/TabbedActivity.kt
@@ -1,16 +1,16 @@
 package com.example.huaweichallange.ui
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.example.huaweichallange.R
 import com.example.huaweichallange.fragment.MapFragment
+import com.example.huaweichallange.fragment.ProfileFragment
+import com.example.huaweichallange.model.UserInfoModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TabbedActivity : AppCompatActivity() {
@@ -23,73 +23,46 @@ class TabbedActivity : AppCompatActivity() {
         val viewPager: ViewPager = findViewById(R.id.viewPager)
 
         val mapButton = findViewById<Button>(R.id.floatButto) as FloatingActionButton
-        val personName = findViewById<View>(R.id.personName) as? TextView
-
-        /*  val personInfo = intent.extras!!.get("extra") as UserInfoModel
- 
-          val bundle = Bundle()
-          bundle.putString("photo", personInfo.personPhoto)
-          bundle.putString("email", personInfo.personName)
-          val myFragment = ProfileFragment()
-          myFragment.arguments = bundle
-          supportFragmentManager.beginTransaction()
-              .replace(R.id.content_id, myFragment).commit()*/
 
 
-        val listOfFragments = listOf(MapFragment())
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
 
-        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager,listOfFragments)
+        viewPagerAdapter.addFragment(MapFragment(), "Map")
 
-//        viewPagerAdapter.addFragment(MapFragment(), "Map")
-//        viewPagerAdapter.addFragment(ProfileFragment(), "Profile")
-//        viewPagerAdapter.addFragment(SettingFragment(), "Settings")
+        val personInfo = intent.extras!!.get("extra") as UserInfoModel
+        val bundle = Bundle()
+        bundle.putString("photo", personInfo.personPhoto)
+        bundle.putString("name", personInfo.personName)
+
+        val profileFragment = ProfileFragment()
+        profileFragment.arguments = bundle
+        viewPagerAdapter.addFragment(profileFragment, "Profile")
 
         viewPager.adapter = viewPagerAdapter
 
 
     }
 
-//    internal class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
-//        private val fragments: ArrayList<Fragment> = ArrayList<Fragment>()
-//        private val titles: ArrayList<String> = ArrayList<String>()
-//        override fun getItem(position: Int): Fragment {
-//            return fragments[position]
-//
-//        }
-//
-//        override fun getCount(): Int {
-//            return fragments.size
-//        }
-//
-//        fun addFragment(fragment: Fragment, title: String) {
-//            fragments.add(fragment)
-//            titles.add(title)
-//        }
-//
-//        override fun getPageTitle(position: Int): CharSequence? {
-//            return titles[position]
-//        }
-//
-//    }
-}
+    internal class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+        private val fragments: ArrayList<Fragment> = ArrayList<Fragment>()
+        private val titles: ArrayList<String> = ArrayList<String>()
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
 
-
-class  ViewPagerAdapter(fm: FragmentManager, private val listOfFragments: List<Fragment>) : FragmentStatePagerAdapter(fm) {
-
-    override fun getItem(position: Int): Fragment {
-        for (fragment in listOfFragments) {
-            val bundle = Bundle()
-            bundle.putString("data1", "PUT_YOUR_DATA_HERE")
-            bundle.putString("data2", "PUT_YOUR_DATA_HERE")
-            fragment.arguments = bundle
-            return fragment
         }
 
-        return Fragment()
-    }
+        override fun getCount(): Int {
+            return fragments.size
+        }
 
-    override fun getCount(): Int {
-        return listOfFragments.count()
-    }
+        fun addFragment(fragment: Fragment, title: String) {
+            fragments.add(fragment)
+            titles.add(title)
+        }
 
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titles[position]
+        }
+
+    }
 }
