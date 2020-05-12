66630a11b5837505816fe04c5ddfb3b1c8135c31
Yusuf Caner Güreş <gurescaner@gmail.com>
2020-05-13 02:39:18 +0300

    Google maps a toast for coordinates added.

--- app/src/main/java/com/example/huaweichallange/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/huaweichallange/fragment/ProfileFragment.kt
@@ -1,6 +1,8 @@
 package com.example.huaweichallange.fragment
 
+
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,7 @@ import android.widget.Button
 import android.widget.Switch
 import androidx.fragment.app.Fragment
 import com.example.huaweichallange.R
+import com.example.huaweichallange.ui.MapsActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 
@@ -16,7 +19,7 @@ import kotlinx.android.synthetic.main.fragment_profile.view.*
 /**
  * A simple [Fragment] subclass.
  */
-class ProfileFragment : Fragment() {
+class ProfileFragment : androidx.fragment.app.Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_profile, container, false)
@@ -29,15 +32,20 @@ class ProfileFragment : Fragment() {
 
         switchButton.setOnCheckedChangeListener{ buttonView, isChecked ->
             if(isChecked){
-                val dark = "true"
+                val dark = "darkModeOn"
                 val pref =
                 activity!!.getSharedPreferences("darkMode", Context.MODE_PRIVATE)
                 val edt = pref.edit()
                 edt.putString("darkMode", dark)
                 edt.commit()
 
+                //val intent = Intent(this@ProfileFragment.context,MapsActivity::class.java)
+                //val bundle =  Bundle()
+                //bundle.putString("darkMode", dark)
+                //intent.putExtras(bundle);
+
             }else{
-                val dark= "false"
+                val dark= "darkModeOff"
 
             }
         }
